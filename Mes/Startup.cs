9780956212ca76a39c66ad9e0e2ded2b2cb6957b
vmezenov <vmezenov@outlook.com>
2019-09-30 13:42:51 +0300

    Добавьте файлы проекта.

---
+++ Mes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mes.Startup))]
+namespace Mes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
