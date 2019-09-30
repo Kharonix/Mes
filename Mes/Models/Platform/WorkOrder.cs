db308434e7dbd02859c2c3a80ee1ee90b0b703af
vmezenov <vmezenov@outlook.com>
2019-09-30 14:02:00 +0300

    !

---
+++ Mes/Models/Platform/WorkOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mes.Models.Platform
+{
+    public class WorkOrder
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int AssemblyId { get; set; }
+        public int CustomerId { get; set; }
+        public int WorkplaceId { get; set; }
+
+        public decimal Count { get; set; }
+        public decimal? DoneCount { get; set; }
+         
+
+
+    }
+}
---
+++ Mes/Models/Platform/WorkOrderContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mes.Models.Platform
+{
+    public class WorkOrderContext : DbContext
+    {
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+        public DbSet<Assembly> Assemblies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Workplace> Workplaces { get; set; }
+    }
+}
