64e8a3334f57224242cc58a3cb0fe52d62921bc8
Владислав Мезенов <xabka99@gmail.com>
2021-03-07 02:08:13 +0300

    !

---
+++ MesTests/Controllers/WorkOrdersControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mes.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mes.Controllers.Tests
+{
+    [TestClass()]
+    public class WorkOrdersControllerTests
+    {
+        [TestMethod()]
+        public void WorkOrdersControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StartedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConfirmedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetViewBagTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
